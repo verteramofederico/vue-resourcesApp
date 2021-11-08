79146b328d2bb14076e638089ed9eff8a80aaf60
verteramo.federico <verteramo.federico@gmail.com>
2021-11-08 19:33:36 -0300

    basic settings

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+    <the-header title="RememberMe"></the-header>
+    <the-resources></the-resources>
+    </div>
+</template>
+
+<script>
+import TheHeader from './components/layouts/TheHeader.vue'
+import TheResources from './components/learning-resources/TheResources.vue'
+
+export default {
+    components: {
+        TheHeader,
+        TheResources
+    }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+    <base-card>
+    <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
+    </base-card>
+    <keep-alive>
+        <component :is="selectedTab"> </component>
+    </keep-alive>
+
+    </div>
+
+</template>
+
+<script>
+
+import StoredResources from './StoredResources.vue'
+import AddResource from './AddResource.vue'
+
+export default {
+    components: {
+        StoredResources,
+        AddResource
+    },
+    data() {
+        return {
+            selectedTab: 'stored-resources',
+            storedResources: [
+                {  
+                id: '1',
+                title: 'Official guide',
+                descripction: 'docs',
+                link: 'www.vue.org'
+                },{
+                id: '2',
+                title: 'google',
+                descripction: 'forums',
+                link: 'www.google.org'   
+                }
+            ]
+        }
+        
+    },
+    provide() {
+        return {
+            resources: this.storedResources,
+            addResource: this.addResource
+        }
+    },
+    computed: {
+    storedResButtonMode() {
+    return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+    return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
+    methods: {
+        setSelectedTab (tab) {
+            this.selectedTab = tab
+        },
+        addResource(title, description, url) {
+            const newResource = {
+                id: new Date().toISOString(),
+                title: title,
+                description: description,
+                link: url
+            }
+            this.storedResources.unshift(newResource)
+            this.selectedTab = 'stored-resources'
+        }
+    }
+}
+</script>
